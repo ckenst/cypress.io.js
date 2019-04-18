08ebcc8b3a676c8d595b5f815ca63039d993ff37
Chris Kenst <ckenst@gmail.com>
2019-04-18 16:33:51 -0700

    Brand new installation with instructions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Cypress
 
 Playing around with [CypressIO](https://www.cypress.io/). The more tools we learn, the better chance we have at recognizing when they are valuable.  
+
+## How to run
+
+Start the Cypress Test Runner `npm run cypress:open`
+
+Select one of the tests in the runner and a browser from the list to start
---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,41 @@
+describe('My First Test', function() {
+  it('finds the content "type"', function() {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type')
+  })
+})
+
+describe('My Second Test', function() {
+  it('Gets, types and asserts', function() {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
+
+describe('My Third Test', function() {
+  it('clicking "type" shows the right headings', function() {
+    cy.visit('https://example.cypress.io')
+
+    cy.pause()
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
