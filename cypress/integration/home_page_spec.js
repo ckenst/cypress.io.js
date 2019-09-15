93a866d77afa8970b64c69195416750b183a4e8a
Chris Kenst <ckenst@gmail.com>
2019-09-14 17:10:01 -0700

    New tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,6 +1,8 @@
+// removed "baseUrl" config from cypress.json
+
 describe('TC.org Home Page', function() {
     it('successfully loads', function() {
-        cy.visit('/')
+        cy.visit('http://localhost:4000/')
         cy.task('hello', { greeting: 'Hello', name: 'World' });
     });
 });
