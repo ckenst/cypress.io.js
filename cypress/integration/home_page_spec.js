3a85fd886543870965ffb23df4defeb3745f14f7
Chris Kenst <ckenst@gmail.com>
2020-01-09 19:06:50 -0800

    Installed Prettier. Updated cypress version

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,7 +2,7 @@
 
 describe('TC.org Home Page', function() {
     it('successfully loads', function() {
-        cy.visit('http://localhost:4000/')
+        cy.visit('http://localhost:4000/');
         cy.task('hello', { greeting: 'Hello', name: 'World' });
     });
 });
--- cypress/integration/todomvc-filter.spec.js
+++ cypress/integration/todomvc-filter.spec.js
@@ -1,32 +1,30 @@
-import { TodoPage } from "../pageobjects/page-todo"
+import { TodoPage } from '../pageobjects/page-todo';
 
-describe('filtering', () =>{
-
+describe('filtering', () => {
     beforeEach(() => {
-        cy.visit('http://todomvc-app-for-testing.surge.sh/')
-        
-        cy.get('.new-todo').type("Clean room{enter}")
-        cy.get('.new-todo').type("Learn JS{enter}")
-        cy.get('.new-todo').type("Use Cypress{enter}")
+        cy.visit('http://todomvc-app-for-testing.surge.sh/');
 
-        cy.get('.todo-list li:nth-child(2) .toggle').click()
-    })
+        cy.get('.new-todo').type('Clean room{enter}');
+        cy.get('.new-todo').type('Learn JS{enter}');
+        cy.get('.new-todo').type('Use Cypress{enter}');
+
+        cy.get('.todo-list li:nth-child(2) .toggle').click();
+    });
 
     it('should filter "Active" todos', () => {
-        cy.contains('Active').click()
+        cy.contains('Active').click();
 
-        cy.get('.todo-list li').should('have.length', 2)
-    })
-    
+        cy.get('.todo-list li').should('have.length', 2);
+    });
+
     it('should filter "Completed" todos', () => {
-        cy.contains('Completed').click()
+        cy.contains('Completed').click();
 
-        cy.get('.todo-list li').should('have.length', 1)
-    })
+        cy.get('.todo-list li').should('have.length', 1);
+    });
     it('should filter "All" todos', () => {
-        cy.contains('All').click()
+        cy.contains('All').click();
 
-        cy.get('.todo-list li').should('have.length', 3)
-    })
-
-})
+        cy.get('.todo-list li').should('have.length', 3);
+    });
+});
